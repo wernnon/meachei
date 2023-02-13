9c116792ffec035cc3b0ed800fcf2e11d0aa3823
wernnon <ruizwernnon@gmail.com>
2023-02-13 08:53:28 -0400

    atualização componentes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import home from './home.png';
-import logo from './logo.png'
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className='outer'>
-          <img src={home} className="App-logo" alt="logo" />
-        </div>
-        <a>
-          <img src={logo} className='publisher'/>
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
