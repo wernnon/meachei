a33d6a96423266ccde2c7fd053af27fdcca79ed5
wernnon <ruizwernnon@gmail.com>
2023-02-15 09:06:23 -0400

    imagem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Logo1 from './Images/Logo1.png'
+import Imagem from './Componentes/Imagem';
 function App() {
   return (
     <div>
-      
+      <Imagem
+      foto={Logo1} />
     </div>
   );
 }
