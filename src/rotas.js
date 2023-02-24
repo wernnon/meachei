831838d80a63c91144c3192d8807a7982b6b32a3
wernnon <ruizwernnon@gmail.com>
2023-02-24 15:49:29 -0400

    componetes

--- src/rotas.js
+++ src/rotas.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './Paginas/Login'
+import Login from './Paginas/Login';
+import Cadastro from './Paginas/Cadastro';
 export default function Rotas() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>}/>
+        <Route path='/cadastro' element={<Cadastro />} />
       </Routes>
     </BrowserRouter>
   )
